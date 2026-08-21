00cc775cf355ebeb097c232a00fa3069cfe62856
chillien-shiu <accdechillin@gmail.com>
2026-08-21 16:13:15 +0700

    i

--- Session03/b3.cs
+++ Session03/b3.cs
@@ -7,4 +7,4 @@ class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.UTF8;
-Console.Writeline("Tiền Điện Sinh Hoạt Gia Đình Theo Bậc Thang (EVN)\r\n")
+Console.Writeline("Tiền Điện Sinh Hoạt Gia Đình Theo Bậc Thang (EVN)")
